3adc74a4d0955c27c0fe7f523a8f6dc3d638b3e8
Nina Finageeva <finageeva.nina@mail.ru>
2024-01-31 10:28:01 +0300

    add tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,21 @@
+from src.item import Item
+
+data = Item("Смартфон", 10000, 20)
+
+def test_calculate_total_price():
+    '''Проверяет правильность выполнения метода, который подситывает total price
+    '''
+    assert data.calculate_total_price() == 200000
+    assert data.quantity == 20
+
+
+def test_apply_discount():
+    '''Проверяет правильность выполнения метода, который подситывает установленную скидку для конкретного товара
+    '''
+    data.pay_rate = 0.8
+    data.apply_discount()
+    assert data.price == 8000.0
+
+
+
 """Здесь надо написать тесты с использованием pytest для модуля item."""

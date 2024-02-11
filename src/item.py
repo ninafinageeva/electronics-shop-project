ea9940b2ea4d2ae33407b57cc9fb1760b23ab5e0
Nina Finageeva <finageeva.nina@mail.ru>
2024-02-11 15:23:54 +0300

    private name

--- src/item.py
+++ src/item.py
@@ -13,7 +13,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
@@ -32,3 +32,42 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        '''
+        name геттер
+        '''
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        '''
+        name сеттер
+        value - новое имя, не длиннее 10 символов
+        '''
+        self.__name = value[:10]
+
+    @classmethod
+    def instantiate_from_cvs(cls):
+        '''класс-метод, инициализирующий экземпляры класса
+        "Item" данными из файла .../src/items.csv'''
+        with cls.DATA_DIR.open(newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            cls.all.clear()
+            for row in reader:
+                name = row['name']
+                price = row['price']
+                quantity = row['quantity']
+                cls(name, price, quantity)
+            return cls
+
+    @staticmethod
+    def string_to_number(param):
+        '''Метод, возвращающий число из числа-строки'''
+        return int(float(param))
+
+
+
+
+
